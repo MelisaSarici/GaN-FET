4e2b517b020346f1f3c359b21807de0d2547b358
MelisaSarici <sarici.asena@gmail.com>
2017-06-22 21:34:00 +0200

    On MOSFETmodelling
    I found an article on IEEE about MOSFET modelling on simulink. I learnt
    that an S function needs to be created for modelling the output current
    and input Vgs-Vds voltages using the basic MOSFET current equations. I
    searched on creating an S function first 1 level then 2 level with tlc.
    I tried to write the MATLAB code according to the prescribed template.
    However, I am stuck at a point. where I can not put 2 variables into the
    function and where I can not find those blue boxes to indicate the
    location of the function m file as in the  examples provided in Simulink
    Browser.

---
+++ MOSFET_model.m
@@ -0,0 +1,180 @@
+function MOSFET_model(block)
+%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
+%   The MATLAB S-function is written as a MATLAB function with the
+%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
+%   name of your S-function.
+%
+%   It should be noted that the MATLAB S-function is very similar
+%   to Level-2 C-Mex S-functions. You should be able to get more
+%   information for each of the block methods by referring to the
+%   documentation for C-Mex S-functions.
+%
+%   Copyright 2003-2010 The MathWorks, Inc.
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+setup(block);
+
+end 
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+%%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 2;
+block.NumOutputPorts = 1;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = 1;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+block.OutputPort(1).Dimensions       = 1;
+block.OutputPort(1).DatatypeID  = 0; % double
+block.OutputPort(1).Complexity  = 'Real';
+
+% Register parameters
+block.NumDialogPrms     = 2;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [-1 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
+block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+block.RegBlockMethod('Update', @Update);
+% block.RegBlockMethod('Terminate', @Terminate); % Required
+
+end 
+
+%%
+%% PostPropagationSetup:
+%%   Functionality    : Setup work areas and state variables. Can
+%%                      also register run-time methods here
+%%   Required         : No
+%%   C-Mex counterpart: mdlSetWorkWidths
+%%
+function DoPostPropSetup(block)
+block.NumDworks = 1;
+  
+  block.Dwork(1).Name            = 'Vgs';
+  block.Dwork(1).Dimensions      = 1;
+  block.Dwork(1).DatatypeID      = 0;      % double
+  block.Dwork(1).Complexity      = 'Real'; % real
+  block.Dwork(1).UsedAsDiscState = true;
+  
+  block.Dwork(2).Name            = 'Vds';
+  block.Dwork(2).Dimensions      = 1;
+  block.Dwork(2).DatatypeID      = 0;      % double
+  block.Dwork(2).Complexity      = 'Real'; % real
+  block.Dwork(2).UsedAsDiscState = true;
+ 
+  
+  block.DialogPrm(1).Name   = 'Kn';
+  block.DialogPrm(1).Dimensions  = 1;
+  block.DialogPrm(1).DatatypeID   = 0;
+  block.DialogPrm(1).Data  = 20e-6;
+
+
+  
+  block.DialogPrm(2).Name   = 'Vt';
+  block.DialogPrm(2).Dimensions  = 1;
+  block.DialogPrm(2).Data  = 0.7;
+
+
+end 
+
+%%
+%% Start:
+%%   Functionality    : Called once at start of model execution. If you
+%%                      have states that should be initialized once, this 
+%%                      is the place to do it.
+%%   Required         : No
+%%   C-MEX counterpart: mdlStart
+%%
+function Start(block)
+
+block.Dwork(1).Data = 0;
+block.Dwork(2).Data = 0;
+
+
+end 
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+
+    if block.Dwork(2).Data<(block.Dwork(1).Data - block.DialogPrm(2).Data)
+           block.OutputPort(1).Data = block.DialogPrm(1).Data*(block.Dwork(1).Data - block.DialogPrm(2).Data-block.Dwork(2).Data*0.5)*block.Dwork(2).Data;
+    end
+
+    if block.Dwork(2).Data>=(block.Dwork(1).Data - block.DialogPrm(2).Data)
+        block.OutputPort(1).Data =block.DialogPrm(1).Data*0.5*(block.Dwork(1).Data - block.DialogPrm(2).Data)^2;
+    end
+end
+
+
+%%
+%% Update:
+%%   Functionality    : Called to update discrete states
+%%                      during simulation step
+%%   Required         : No
+%%   C-MEX counterpart: mdlUpdate
+%%
+function Update(block)
+
+block.Dwork(1).Data = block.InputPort(1).Data;
+block.Dwork(2).Data = block.InputPort(2).Data;
+
+
+end
+
+
+
+
