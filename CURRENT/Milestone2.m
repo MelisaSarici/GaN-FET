85eb9fc64f2dd24817f9436dc0f85d58639f5fe2
mesutto <mesutugur@gmail.com>
2017-12-27 17:15:10 +0300

    misafir :))

--- CURRENT/Milestone2.m
+++ CURRENT/Milestone2.m
@@ -8,7 +8,7 @@ Vac=140;        %The ac voltage is 140 V line to line rms
 Vdc=270 ;         % Voltage 
 Vgst=1.7; %typical threshold voltage
 pf=0.9;
-fsw = 80e3;         %Hz - PWM switching frequency for every inverter arm-switch
+fsw = 200e3;         %Hz - PWM switching frequency for every inverter arm-switch
 Vgs_off=0;  
 Vgs=6;
 
@@ -19,7 +19,7 @@ Rd_on=50e-3;  % at the found Ipeak value for rds, 150 Celcius
 Rrev_on=Rd_on; % from the Vds-ýd Graph
 Esw_on=47.5*1e-6;     %J - IGBT's turn-on switching energy per pulse at Ic (Icp, T=125C) found from curve
 Esw_off=7.5*1e-6;    %J - IGBT's turn-off switching energy per pulse at Ic (Icp, T=125C) found from curve
-
+Eoss = 7e-6;
 
 Vconduction=Rd_on*Id  ;        %V - IGBT saturation voltage drop @Icp and T=125C
 m_a= 0.85 ;  % - PWM duty factor (Modulation depth)
@@ -34,11 +34,15 @@ Psw=(Esw_on+Esw_off)*fsw*(1/pi);                       %Switching Loss per switc
 
 
 % On State Loss when Vgs>0 
-    Prev1=Id^2*Rd_on;
+    Prev1=Id*Vconduction*((1/8)-(m_a/(3*pi))*pf);
 
-%off state loss when Vgs <0 extra loss
-    Prev2=Id^2*Rrev_on + Id*(Vgst+abs(Vgs_off));
+% Off state loss when Vgs <0 extra loss
+    Prev2=(Id^2*Rrev_on + Id*(Vgst+abs(Vgs_off)))*0.01*((1/8)-(m_a/(3*pi))*pf);
 
+% Oss loss
+Poss=(Eoss)*fsw*(1/pi);
 
 %Total Loss
-Ptot=(Pss+Psw+Prev1+Prev2)*6;
+Ptot=(Pss+Psw+Poss+Prev1+Prev2)*6;
+efficiency = RePower/(RePower+Ptot);
+
